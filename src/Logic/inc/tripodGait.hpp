a3aa4ed2196c9148bf47082883a2c9240dc2f33c
Krzysiu <krzysztof.puzio@aetherbiomedical.com>
2023-06-18 04:17:00 +0200

    Tripod gait

--- src/Logic/inc/tripodGait.hpp
+++ src/Logic/inc/tripodGait.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "gaitInterface.hpp"
+#include "constants.hpp"
 
 namespace logic::gait {
     class TripodGait : public GaitInterface {
@@ -11,5 +12,24 @@ namespace logic::gait {
         Result TurnLeft() override;
         Result GoToTheDefaultPosition() override;
         Gait GetCurrentGait() override { return TRIPOD_WALK; };
-    }
+    private:
+        typedef enum {
+            MOVE_LIFTED_LEGS_TO_THE_DEFAULT_X_POSITION,
+            LIFT_LEGS_IN_INCORRECT_POSITION,
+            MOVE_LEGS_DOWN,
+            LEGS_IN_DEFAULT_POSITION
+        }DefaultPositionSteps;
+        float xForeward = Constants::X_ABSOLUTE_RANGE[0];
+        float xBackwards = Constants::X_ABSOLUTE_RANGE[1];
+        uint8_t firstGroupOfLegs[3] = {
+            leg::LegContainer::LEFT_FRONT,
+            leg::LegContainer::RIGHT_MIDDLE,
+            leg::LegContainer::LEFT_BACK
+        };
+        uint8_t secondGroupOfLegs[3] = {
+            leg::LegContainer::RIGHT_FRONT,
+            leg::LegContainer::LEFT_MIDDLE,
+            leg::LegContainer::RIGHT_BACK
+        };
+    };
 } // namespace logic::gait
--- src/Logic/src/tripodGait.cpp
+++ src/Logic/src/tripodGait.cpp
@@ -1,23 +1,60 @@
 #include "tripodGait.hpp"
 
 namespace logic::gait {
-    Result TripodGait::GoToTheDefaultPosition() {
+    Result TripodGait::GoForeward() {
         typedef enum {
-            MOVE_LIFTED_LEGS_TO_THE_DEFAULT_X_POSITION,
-            LIFT_LEGS_IN_INCORRECT_POSITION,
-            MOVE_LEGS_DOWN,
-            LEGS_IN_DEFAULT_POSITION
-        }DefaultPositionSteps;
+            LIFT_LEGS_AND_MOVE_THEM_FORWARD,
+            LOWER_LEGS_MOVE_THEM_BACKWARD
+        } FirstGroupOfLegsSteps;
         switch (step) {
-        case MOVE_LIFTED_LEGS_TO_THE_DEFAULT_X_POSITION:
+        case LIFT_LEGS_AND_MOVE_THEM_FORWARD: {
+            for (int i : firstGroupOfLegs) {
+                targetLegsPositions.legs[i].x = xForeward;
+                targetLegsPositions.legs[i].footOnGround = false;
+            }
+            for (int i : secondGroupOfLegs) {
+                targetLegsPositions.legs[i].x = xBackwards;
+                targetLegsPositions.legs[i].footOnGround = true;
+            }
+            step = LOWER_LEGS_MOVE_THEM_BACKWARD;
+            break;
+        }
+        case LOWER_LEGS_MOVE_THEM_BACKWARD: {
+            for (int i : firstGroupOfLegs) {
+                targetLegsPositions.legs[i].x = xBackwards;
+                targetLegsPositions.legs[i].footOnGround = true;
+            }
+            for (int i : secondGroupOfLegs) {
+                targetLegsPositions.legs[i].x = xForeward;
+                targetLegsPositions.legs[i].footOnGround = false;
+            }
+            step = LIFT_LEGS_AND_MOVE_THEM_FORWARD;
+            break;
+        }
+        }
+        return RESULT_OK;
+    }
+
+    Result TripodGait::GoToTheDefaultPosition() {
+        switch (step) {
+        case MOVE_LIFTED_LEGS_TO_THE_DEFAULT_X_POSITION: {
+            bool allLegsOnTheGround = true;
             for (leg::FootTargetPosition& leg : targetLegsPositions.legs) {
                 if (!leg.footOnGround) {
                     leg.x = 0;
+                    allLegsOnTheGround = false;
+                }
+            }
+            if (allLegsOnTheGround) {
+                for (int i : firstGroupOfLegs) {
+                    targetLegsPositions.legs[i].x = 0;
+                    targetLegsPositions.legs[i].footOnGround = false;
                 }
             }
             step = MOVE_LEGS_DOWN;
             break;
-        case LIFT_LEGS_IN_INCORRECT_POSITION:
+        }
+        case LIFT_LEGS_IN_INCORRECT_POSITION: {
             for (leg::FootTargetPosition& leg : targetLegsPositions.legs) {
                 if (leg.x != 0) {
                     leg.footOnGround = false;
@@ -25,7 +62,8 @@ namespace logic::gait {
             }
             step = MOVE_LIFTED_LEGS_TO_THE_DEFAULT_X_POSITION;
             break;
-        case MOVE_LEGS_DOWN:
+        }
+        case MOVE_LEGS_DOWN: {
             bool allLegsAreInTheirDefaultPosition = true;
             for (leg::FootTargetPosition& leg : targetLegsPositions.legs) {
                 leg.footOnGround = true;
@@ -40,6 +78,7 @@ namespace logic::gait {
                 step = LIFT_LEGS_IN_INCORRECT_POSITION;
             }
             break;
+        }
         case LEGS_IN_DEFAULT_POSITION:
             return RESULT_LEG_IN_TARGET_POSITION;
         default:
@@ -47,4 +86,5 @@ namespace logic::gait {
         }
         return RESULT_OK;
     }
+
 } // namespace logic::gait
