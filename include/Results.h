fcb21f96b6d8d9a91debec40a6e6b65f1107397f
Krzysiu <krzysztof.puzio@aetherbiomedical.com>
2023-06-22 22:49:37 +0200

    ReturnOnError and new results

--- include/Results.h
+++ include/Results.h
@@ -1,11 +1,26 @@
 #pragma once
 
+#define ReturnOnError(result) ({ \
+    Result r = result; \
+    if (r != RESULT_OK) \
+        return r; \
+})
+
+#define ReturnUnexpected(result, unexpectedResult) ({ \
+    Result r = result; \
+    if (r == unexpectedResult) \
+        return r; \
+})
+
 typedef enum Result {
     RESULT_OK,
 
     RESULT_SERVO_IN_TARGET_POSITION,
     RESULT_SERVO_MOVING,
     RESULT_SERVO_VELOCITY_EQ_0,
+    RESULT_SERVO_VELOCITY_OUT_OF_RANGE,
+    RESULT_SERVO_ANGLE_OUT_OF_RANGE,
+
 
     RESULT_COORDINATES_OUT_OF_RANGE,
     RESULT_HIGHT_OUT_OF_RANGE,
@@ -17,6 +32,7 @@ typedef enum Result {
     RESULT_LEGS_NOT_IN_DEFAULT_POSITION,
 
     RESULT_WRONG_GAIT,
+    RESULT_DIRECTION_NOT_CHOSEN,
 
     RESULT_UNDEFINED_ERROR
 }Result;
