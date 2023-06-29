d4b5feb2ab5034791b9c38d7071bf013676f2cb5
Krzysiu <krzysztof.puzio@aetherbiomedical.com>
2023-06-29 22:32:01 +0200

    fix python script

--- test_generator.py
+++ test_generator.py
@@ -1,14 +1,31 @@
 import os
 
-print("Choose test type:\r\n1. System\r\n2. Hardware")
+
+def UpdateMainCMakesLists(path):
+    dirs = os.listdir(path)
+    dirs = [d for d in dirs if os.path.isdir(os.path.join(path, d))]
+    print(dirs)
+
+    with open(f"{path}/CMakeLists.txt", "w") as f:
+        for dir in dirs:
+            f.write(f"add_subdirectory({dir})\n")
+
+
+print("Choose test type:\r\n1. System\r\n2. Hardware \r\n3. Refresh CMakeLists.txt")
 test_type = input()
 test_system_path = f"./test_system"
 test_hardware_path = f"./test_hardware_drivers"
 match test_type:
     case "1":
         path = test_system_path
+        target_link_libraries = "LOGIC\n\tDRIVERS"
     case "2":
         path = test_hardware_path
+        target_link_libraries = "DRIVERS"
+    case "3":
+        UpdateMainCMakesLists(test_system_path)
+        UpdateMainCMakesLists(test_hardware_path)
+        exit()
     case _:
         print("Wrong test type")
         exit()
@@ -17,13 +34,13 @@ print("Write new test name:")
 test_name = input()
 
 try:
-    os.mkdir(f"{path}/{test_name}")
+    os.mkdir(f"{os.path.join(path,test_name)}")
 except OSError:
     print("Directory already exists")
     exit()
-f = open(f"{path}/{test_name}/{test_name}.cpp", "a")
-f.write(
-    """
+with open(f"{path}/{test_name}/{test_name}.cpp", "w") as f:
+    f.write(
+        """
 #include <stdio.h>
 #include "pico/stdlib.h"
 int main(){
@@ -35,12 +52,11 @@ int main(){
     return 0;
 }
 """
-)
-f.close()
+    )
 
-f = open(f"{path}/{test_name}/CMakeLists.txt", "a")
-f.write(
-    f"""
+with open(f"{path}/{test_name}/CMakeLists.txt", "w") as f:
+    f.write(
+        f"""
 include($ENV{{PICO_SDK_PATH}}/external/pico_sdk_import.cmake)
 pico_sdk_init()
 
@@ -49,8 +65,7 @@ project({test_name} C CXX)
 add_executable({test_name} {test_name}.cpp)
 
 target_link_libraries({test_name} 
-    LOGIC
-    DRIVERS
+    {target_link_libraries}
 ) 
 
 pico_enable_stdio_usb({test_name} 1)
@@ -59,8 +74,6 @@ pico_enable_stdio_uart({test_name} 0)
 #Generates different file formats
 pico_add_extra_outputs({test_name})
     """
-)
-f.close()
+    )
 
-f = open(f"{path}/{test_name}/CMakeLists.txt", "a")
-f.write(f"\r\nadd_subdirectory({test_name})")
+UpdateMainCMakesLists(path)
