8726a8cd57df13976504933daf956028d9953839
Krzysiu <krzys.p9@gmail.com>
2023-07-13 17:19:40 +0200

    test_mcp3008 fix

--- test_hardware_drivers/test_mcp3008/test_mcp3008.cpp
+++ test_hardware_drivers/test_mcp3008/test_mcp3008.cpp
@@ -9,20 +9,22 @@
 #define RX_PIN 4
 #define CS_PIN 5
 
-void UpdateADC(std::array<uint16_t, 8> &adc_values, pico_drivers::MCP3008 &adc_spi_0);
-void PrintADC(const std::array<uint16_t, 8> const& adc_values);
+void UpdateADC(std::array<uint16_t, 8>& adc_values, pico_drivers::MCP3008& adc_spi_0);
+void PrintADC(const std::array<uint16_t, 8>& adc_values);
 
 int main() {
     stdio_init_all();
     using namespace pico_drivers;
-    Gpio gpio = Gpio(25, Gpio::OUTPUT);
+    Gpio led = Gpio(25, Gpio::OUTPUT);
+    Gpio gpio = Gpio(0, Gpio::OUTPUT);
     MCP3008 adc_spi_0 = MCP3008(SPI_INSTANCE, BAUD_RATE, SCK_PIN, TX_PIN, RX_PIN, CS_PIN);
     std::array<uint16_t, 8> adc_values;
-    
+
     while (1) {
         UpdateADC(adc_values, adc_spi_0);
         PrintADC(adc_values);
         gpio.Toggle();
+        led.Toggle();
         sleep_ms(1000);
     }
 }
@@ -32,7 +34,7 @@ void UpdateADC(std::array<uint16_t, 8>& adc_values, pico_drivers::MCP3008& adc_s
         adc_values[i] = adc_spi_0.ReadChannel(i);
     }
 }
-void PrintADC(const std::array<uint16_t, 8> const& adc_values) {
+void PrintADC(const std::array<uint16_t, 8>& adc_values) {
     for (int i = 0; i < adc_values.size(); i++) {
         printf("ADC %d value: %d\n", i, adc_values[i]);
     }
